c0911427bf364b87ce40c17aa3bcef146fd2a51a
Alex Crane <alexcranedesigner@gmail.com>
2019-03-17 18:35:40 -0700

    loading data from profiles json file

--- src/index.js
+++ src/index.js
@@ -2,29 +2,78 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import scss from '../styles/index.scss';
+import profiles from './data/profiles.json';
 
-class StaticImage extends Component {
-  clickButton() {
-    // System.import splits bundle.js files for efficiency
-    System.import('./static_image').then(module => {});
+class App extends Component {
+  state = {
+    users: [
+        {
+          "id": 1,
+          "artistName": "Nils Frahm",
+          "location": "Germany",
+          "genres": ["Dance", "Electronic", "Ambient"]
+        },
+        {
+          "id": 2,
+          "artistName": "Deftones",
+          "location": "USA",
+          "genres": ["Metal", "Nu Metal", "Rock"]
+        }
+    ]
   }
+}
 
+class Profile extends Component {
   render() {
+    const profileId = this.props.match.params.id;
+
+    /*const profile = profiles.map((profile, i) => {
+          let curProfile = profiles[i];
+          if(curProfile['id'] === 1){
+              return curProfile
+          }
+    });*/
+
     return (
       <div>
-        <h2>Render static image</h2>
-        <button onClick={this.clickButton}>Show image</button>
-        <div className="luna-wrapper" />
+        <h2>Profile: {profiles[profileId - 1].artistName}</h2>
+        {console.log(profiles[profileId - 1].artistName)}
       </div>
     );
   }
 }
 
-class Unsplash extends Component {
+class Profiles extends Component {
+  render() {
+    const profileElement = profiles.map((profile, i) => {
+        return (
+          <div key={i}>
+            <Link
+                to={{ pathname: '/' + profile.id}}
+                key={profile.id}>
+                {profile.artistName}
+            </Link>
+          </div>
+        )
+    });
+    return (
+      <div>
+          <h1>Profiles</h1>
+          <div className="list-group">
+            <div>
+              {profileElement}
+            </div>
+          </div>
+      </div>
+    );
+  }
+}
+
+class Releases extends Component {
   render() {
     return (
       <div>
-        <h2>Unsplash images</h2>
+        <h2>Releases</h2>
       </div>
     );
   }
@@ -35,17 +84,21 @@ ReactDOM.render(
     <div>
       <ul>
         <li>
-          <Link to="/">Static Image</Link>
+          <Link to="/">Profiles</Link>
         </li>
         <li>
-          <Link to="/unsplash-feed">Unsplash Images</Link>
+          <Link to="/Releases">Releases</Link>
         </li>
       </ul>
 
       <div>
         <Switch>
-          <Route path="/unsplash-feed" component={Unsplash} />
-          <Route path="/" component={StaticImage} />
+          <Route path="/Releases" component={Releases} />
+          <Route
+            path="/:id"
+            component={Profile}
+          />
+          <Route path="/" component={Profiles} />
         </Switch>
       </div>
     </div>
